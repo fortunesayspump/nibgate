e6cbfbf5d534750e5654a8d4c02b245a626e25d2
Fortune <fortuneze0@gmail.com>
2026-07-27 19:51:40 +0100

    add charset utf-8 meta tag to subblog layout

--- subblogs/frontend/src/app/layout.tsx
+++ subblogs/frontend/src/app/layout.tsx
@@ -44,6 +44,7 @@ export default async function RootLayout({ children }: { children: React.ReactNo
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <meta charSet="utf-8" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Kumbh+Sans:wght@400;500;600;700&display=swap" rel="stylesheet" />
